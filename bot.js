31da60e3d06b833d8101ca87ee71699fa63c8a3d
LoganthegodOG <stevensmiind@gmail.com>
2017-12-11 17:21:30 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -35,13 +35,58 @@ bot.on("message", async message =>{
 	}
 
 	if(command === `${prefix}mute`) {
-		if(message.memeber.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You do not have the permissions to manage messages");
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You dont have the permission to do so!");
+			
 		
-		let toMute = message.mentions.users.first() || message.guild.members.get(args[0]);
+		let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 		if(!toMute) return message.channel.sendMessage("You did not specify a user mention or ID!");
 
-		return message.reply(toMute.username || toMute.user.username);
+		let role = message.guild.roles.find(r => r.name === "Agro Muted");
+		if(!role) {
 
+		}
+		try{
+		role = await message.guild.createRole({
+		name: "Agro Muted",
+		color: "##9e346f"
+
+		});
+
+			message.guild.channels.forEach(async (channel,id) => {
+				await channel.overwritePermissions(role, {
+					SEND_MESSAGES: false,
+					ADD_REACTIONS: false
+
+				});
+			});
+		}	catch(e){
+			console.log(e.stack);
+			}
+		
+		if(toMute.roles.has(role.id)) return message.channel.sendMessage("This user already is muted");
+
+		await toMute.addRole(role);
+		message.channel.sendMessage("That User Has Been Muted");
+
+		return;
+
+	}
+	if(command === `${prefix}unmute`) {
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You dont have the permission to do so!");
+			
+		
+		let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+		if(!toMute) return message.channel.sendMessage("You did not specify a user mention or ID!");
+
+		let role = message.guild.roles.find(r => r.name === "Agro Muted");
+		
+	
+		if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("This user is not muted");
+
+		await toMute.removeRole(role);
+		message.channel.sendMessage("That User Has Been Unmuted");
+
+		return;
 
 	}
 	
